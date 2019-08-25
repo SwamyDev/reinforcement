6241fbc23e233d431bc7d0281562ca905cba1a15
SwamyDev <b.raml@gmx.at>
2019-08-25 11:04:34 +0200

    Run make meta from within setup.py
    Travis CI doesn't seem to keep the _version.py file when building the
    meta target. This target is now built automatically when setup.pu is run

--- setup.py
+++ setup.py
@@ -3,13 +3,19 @@ See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
-
+import subprocess
+import sys
 from codecs import open
 from pathlib import Path
 
 from setuptools import setup, find_packages
 
 here = Path(__file__).absolute().parent
+
+error = subprocess.call(['make', 'meta'])
+if error:
+    print(f"failed to run 'make meta' with error: {error}")
+    sys.exit(-1)
 
 with open(here / Path('README.md'), encoding='utf-8') as f:
     long_description = f.read().replace('\r\n', '\n')
